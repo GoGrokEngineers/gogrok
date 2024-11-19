e298101bb7292d3f2178d3b7187e0445739a5929
feruzbek23 <feruzbek.dev@gmail.com>
2024-11-19 12:33:53 +0100

    Change settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-07-hd3b$s!82$(w^j3@qu%p#zm(p^0vpi2da_d!1ioi%8u78s&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', "139.162.134.90", "localhost"]
 
