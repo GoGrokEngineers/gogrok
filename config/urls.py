a853b820d0383d46c66bb7a096116da1f567b934
feruzbek23 <feruzbek.dev@gmail.com>
2024-12-11 13:00:52 +0100

    Update Swagger settings

--- config/urls.py
+++ config/urls.py
@@ -19,16 +19,16 @@ from config import settings
 from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Your API",
+        title="API",
         default_version="v1",
-        description="Description of your API",
-        terms_of_service="https://www.google.com/policies/terms/",
         license=openapi.License(name="BSD License"),
     ),
     public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
