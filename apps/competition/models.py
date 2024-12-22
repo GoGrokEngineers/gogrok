6c2dedc8bbc00deddc177de173614a3982a23f26
Musoxon Qaraxujayev <musoxonqaraxujayev@gmail.com>
2024-12-22 10:48:35 +0100

    testing updatable dependec ies

--- apps/competition/models.py
+++ apps/competition/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class CompetitionStatisticsModel(models.Model):
     date = models.DateField(auto_now=True)
-    tasks = models.ManyToManyField("task.models.Task")
+    tasks = models.ManyToManyField("task.Task")
     total_competitions = models.IntegerField(default=0)
     
